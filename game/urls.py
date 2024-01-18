e861604030c83c9330f1bb85512c256715a09c18
shj <hajong1206@gmail.com>
2024-01-18 16:13:02 +0900

    git pull 받기

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
+from .views import *
 
 app_name = 'game'
 
 urlpatterns = [
+    path('main/', main, name='main'),
+    path('game_create/', game_create, name='game_create'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import GameForm
 
-# Create your views here.
+def main(request):
+    return render(request, template_name='game/main.html')
+
+def game_create(request):
+    form = GameForm()
+    ctx = {'form': form}
+    return render(request, template_name='game/game_create.html', context=ctx)
