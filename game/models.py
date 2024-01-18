f1c3620d1ef01f289f35f23eb0aab53e11446525
yeonuKim <ywk0524@snu.ac.kr>
2024-01-18 17:18:32 +0900

    [feat] Add game create

--- game/models.py
+++ game/models.py
@@ -6,6 +6,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Game(models.Model):
     rule = models.BooleanField()
     attacker_card = models.PositiveIntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)])
-    defender_card = models.PositiveIntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)])
+    defender_card = models.PositiveIntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)], null=True, blank=True)
     attacker=models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank=True, related_name='attack_game')
     defender=models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank=True, related_name='defend_game')
