9ce70581042c60b4003c047e716652d807856bd1
shash0423 <shash042319@naver.com>
2024-01-18 17:29:10 +0900

    [feat] 게임 리스트 초기작업

--- game/urls.py
+++ game/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from .views import *
 
 app_name = 'game'
 
 urlpatterns = [
+    path('list/', game_list, name='game_list'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Game
 
 # Create your views here.
+def game_list(request):
+    #Game 객체 전부 가져오기
+    games = Game.objects.all()
+
+    ctx = {'games':games}
+    return render(request, 'game/game_list.html', ctx)
