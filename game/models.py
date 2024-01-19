a7f2a976d0674964ab4f1a5a0bf5095d61272614
yeonuKim <ywk0524@snu.ac.kr>
2024-01-19 12:20:12 +0900

    [feat] Add created_date in game model and change sorting index

--- game/models.py
+++ game/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from common.models import User
+from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
@@ -9,3 +10,4 @@ class Game(models.Model):
     defender_card = models.PositiveIntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)], null=True, blank=True)
     attacker=models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank=True, related_name='attack_game')
     defender=models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank=True, related_name='defend_game')
+    created_date = models.DateTimeField(default=timezone.now)
